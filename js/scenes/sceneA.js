485fd979af3b5adff331fd9f3c7e9bdf606cf57b
Gerardo Belot <gbelot2003@hotmail.com>
2022-09-03 14:26:31 -0600

    🔧 Usoy manejo de esenas

---
+++ js/scenes/sceneA.js
@@ -0,0 +1,36 @@
+class SceneA extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: "SceneA"
+        });
+    }
+
+    preload(){
+
+    }
+
+    create(){
+        let graphics = this.add.graphics();
+        graphics.fillStyle(0xff33);
+        graphics.fillRect(100, 200, 600, 300);
+        graphics.fillRect(100, 100, 100, 100);
+
+        this.add.text(120, 110, "A", {
+            font: "96px Courier",
+            fill: "black"
+        })
+
+        this.scene.add("SceneC", new SceneC)
+        //this.scene.start("SceneC")
+
+        //this.scene.bringToTop("SceneA")
+        this.scene.sendToBack("SceneC")
+
+
+    }
+
+    update(time, delta){
+
+    }
+}
