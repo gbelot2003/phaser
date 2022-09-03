e2c6b9eff6e1764a44166f031f135b55a588ff2d
Gerardo Belot <gbelot2003@hotmail.com>
2022-09-03 13:14:00 -0600

    🔧 Agregando movimientos al sprit

--- js/init.js
+++ js/init.js
@@ -21,18 +21,47 @@ const config = {
 let game = new Phaser.Game(config);
 
 function preload (){
-    this.load.image("pajaro", "./assets/bird.png");
+    this.load.image("bird", "./assets/bird.png");
 }
 
 function create (){
-    this.pajaro = this.physics.add.image(50, 100, "pajaro");
-    this.pajaro.setScale(2);
-    this.pajaro.flipX = false;
-    this.pajaro.setAngle(0);
-    this.pajaro.setOrigin(0)
-    this.pajaro.setCollideWorldBounds(true);
-    this.pajaro.setBounce(0.5);
-    this.pajaro.setVelocity(50, 0)
+    this.pajaro = this.add.image(50, 100, "bird");
+    // this.pajaro.setScale(2);
+    // this.pajaro.flipX = false;
+    //this.pajaro.setAngle(0);
+    // this.pajaro.setOrigin(0)
+    //this.pajaro.setCollideWorldBounds(true);
+    //this.pajaro.setBounce(0.5);
+    //this.pajaro.setVelocity(50, 0)
+
+    this.input.keyboard.on('keydown-RIGHT', (event) => {
+        console.log(event)
+        this.pajaro.setAngle(0);
+        this.pajaro.flipX = false;
+        this.pajaro.x++ * 2;
+    });
+
+    this.input.keyboard.on('keydown-LEFT', (event) => {
+        console.log(event)
+        this.pajaro.flipX = true;
+        this.pajaro.setAngle(0);
+        this.pajaro.x--
+    });
+
+    this.input.keyboard.on('keydown-UP', (event) => {
+        console.log(event)
+        this.pajaro.flipX = false;
+        this.pajaro.y--
+        this.pajaro.setAngle(-90);
+    });
+
+    this.input.keyboard.on('keydown-DOWN', (event) => {
+        console.log(event)
+        this.pajaro.flipX = false;
+        this.pajaro.y++
+        this.pajaro.setAngle(90);
+    });
+
 
 }
 
