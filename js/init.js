6b5891c0a8d4c8ecc7236de90fcc167e1fcefe0b
Gerardo Belot <gbelot2003@hotmail.com>
2022-09-03 12:41:18 -0600

    🤔 Add physics

--- js/init.js
+++ js/init.js
@@ -7,20 +7,35 @@ const config = {
         preload: preload,
         create: create,
         update: update
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 500
+            }
+        }
     }
 }
 
 let game = new Phaser.Game(config);
 
 function preload (){
-    console.log("soy preload")
     this.load.image("pajaro", "./assets/bird.png");
 }
 
 function create (){
-    console.log("soy create")
-    this.add.image(50, 100, "pajaro");
+    this.pajaro = this.physics.add.image(50, 100, "pajaro");
+    this.pajaro.setScale(2);
+    this.pajaro.flipX = false;
+    this.pajaro.setAngle(0);
+    this.pajaro.setOrigin(0)
+    this.pajaro.setCollideWorldBounds(true);
+    this.pajaro.setBounce(0.5);
+    this.pajaro.setVelocity(50, 0)
+
 }
 
 function update (time, delta){
+    //this.pajaro.x++
 }
